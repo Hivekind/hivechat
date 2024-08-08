80ebc5a69353f95a42e94735b72ce9b2121d053d
parasquid <git@parasquid.com>
2024-08-08 15:00:49 +0800

    feat: :boom: set basepath of the app
    you should now gp to /hivechat in order to access the application (instead of /)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/hivechat",
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
