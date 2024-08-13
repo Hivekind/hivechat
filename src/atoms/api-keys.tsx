ac0dd995b6077c6b6a86e176f2b227365c5f80c9
parasquid <git@parasquid.com>
2024-08-13 15:56:16 +0800

    feat: :construction: add proof of concept encryption and decryption of keys

--- src/atoms/api-keys.tsx
+++ src/atoms/api-keys.tsx
@@ -1,5 +1,10 @@
 import { atom } from "recoil";
 import { AtomEffect } from "recoil";
+import crypto from "crypto";
+
+// temporary secret and iv for encryption
+const secret = "0123456789abcdef0123456789abcdef";
+const iv = "0123456789abcdef";
 
 // see https://stackoverflow.com/a/74256045
 const store = typeof window !== "undefined" ? window.localStorage : null;
@@ -10,13 +15,13 @@ export const localStorageEffect: (key: string) => AtomEffect<any> =
     if (store) {
       const savedValue = store.getItem(key);
       if (savedValue != null) {
-        setSelf(JSON.parse(savedValue));
+        setSelf(JSON.parse(decrypt(savedValue)));
       }
 
       onSet((newValue, _, isReset) => {
         isReset
           ? store.removeItem(key)
-          : store.setItem(key, JSON.stringify(newValue));
+          : store.setItem(key, encrypt(JSON.stringify(newValue)));
       });
     }
   };
@@ -32,3 +37,17 @@ export const geminiApiKeyState = atom({
   default: "",
   effects: [localStorageEffect("geminiApiKey")],
 });
+
+function encrypt(text: string) {
+  const cipher = crypto.createCipheriv("aes-256-cbc", secret, iv);
+  let encrypted = cipher.update(text, "utf8", "hex");
+  encrypted += cipher.final("hex");
+  return encrypted;
+}
+
+function decrypt(encrypted: string) {
+  const decipher = crypto.createDecipheriv("aes-256-cbc", secret, iv);
+  let decrypted = decipher.update(encrypted, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+  return decrypted;
+}
